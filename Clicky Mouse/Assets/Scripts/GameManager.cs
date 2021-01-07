9fbaba768fdf15d3178d55e6426c0342f82cd31b
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-01-07 20:57:06 +0800

    Section 5.2 Under Construction

--- Clicky Mouse/Assets/Scripts/GameManager.cs
+++ Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
     private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -26,6 +31,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
+            UpdateScore(5);
         }
+    }
+
+    void UpdateScore(int ScoreToAdd)
+    {
+        score = score + ScoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
