4df7e2d4131ba2ed81f79dc53c41e464532b80c1
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-01-08 23:28:31 +0800

    Section 5.3 Finish

--- Clicky Mouse/Assets/Scripts/GameManager.cs
+++ Clicky Mouse/Assets/Scripts/GameManager.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
@@ -46,8 +49,13 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
-
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
