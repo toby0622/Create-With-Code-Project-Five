7736011c57e0953f56e2c1a3263186255b82a164
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-01-09 00:18:39 +0800

    Project Finished

---
+++ Clicky Mouse/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gamemanager;
+
+    public int difficulty;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gamemanager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gamemanager.StartGame(difficulty);
+    }
+}
